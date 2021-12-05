66e575bcf17bfc867bd17599655bf930747c5367
elteammate <elteammate@gmail.com>
2021-12-05 20:35:39 +0300

    🚩Make text area dynamically resizable

--- 15-N.Stepanov/shared/components/ui-textarea.js
+++ 15-N.Stepanov/shared/components/ui-textarea.js
@@ -1,5 +1,27 @@
 export default Vue.component('ui-textarea', {
     template: `
-    <textarea class='ui-textarea' autofocus></textarea>
+    <span 
+        class="ui-textarea" 
+        autofocus 
+        @input="onInput"
+        contenteditable
+    >
+        <slot></slot> 
+    </span>
     `,
+    props: {
+        dynamicResize: Boolean,
+        minHeight: Number,
+        maxHeight: Number,
+    },
+    methods: {
+        onInput(e) {
+            if (this.dynamicResize) {
+                const range = document.createRange()
+                range.selectNodeContents(e.target)
+                const targetHeight = range.getBoundingClientRect().height + 50
+                e.target.style.height = `${targetHeight}px`
+            }
+        }
+    }
 })
