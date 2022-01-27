d552ae69a20bacae0d3cfb8e127d3d3aa4fd356a
elteammate <elteammate@gmail.com>
2022-01-27 10:41:10 +0300

    🚩Color snackbar based on action result

--- 15-N.Stepanov/shared/components/ui-snackbar.js
+++ 15-N.Stepanov/shared/components/ui-snackbar.js
@@ -2,13 +2,29 @@ export default Vue.component('ui-snackbar', {
     template: `
         <div class="ui-snackbar">
             <div 
-                class="snackbar-content rounded" 
-                :class="{visible: this.visible}"
+                class="snackbar-content rounded"
+                :class="{
+                        visible: this.visible,
+                        secondary: this.type === 'secondary',
+                        info: this.type === 'info',
+                        success: this.type === 'success',
+                        warning: this.type === 'warning',
+                        error: this.type === 'error',
+                    }"
             >
                 {{this.content}}
             </div>
         </div>
     `,
+
+    props: {
+        type: {
+            validator(x) {
+                return x === undefined ||
+                    ["secondary", "info", "success", "warning", "error"].indexOf(x) !== -1
+            }
+        }
+    },
 
     data() {
         return {
